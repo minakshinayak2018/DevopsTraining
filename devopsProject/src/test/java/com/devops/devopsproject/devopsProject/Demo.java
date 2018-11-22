78450310a7f135074a3d6ebb0efb4ad91ac3c6a3
Minakshi Nayak <M1041228@mindtree.com>
2018-11-22 19:47:34 +0530

    modified pom file

--- devopsProject/src/test/java/com/devops/devopsproject/devopsProject/Demo.java
+++ devopsProject/src/test/java/com/devops/devopsproject/devopsProject/Demo.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 class Demo{    
 		 
-	    @SuppressWarnings("resource")
+	   
 		public static void main(String args[]) throws InterruptedException, IOException{
 	 
 	    	BufferedReader isr = new BufferedReader(new InputStreamReader(System.in));
---
+++ devopsProject/src/test/java/com/devops/devopsproject/devopsProject/WithoutSplit.java
@@ -0,0 +1,36 @@
+package com.devops.devopsproject.devopsProject;
+
+public class WithoutSplit {
+
+	String s = "aaa,bb,g,s,ll";
+	String[] temp = new String[10];
+	String split ="";
+	int length = s.length();
+	int index =0;
+	
+	public void withOutSliptFunction() {
+		for(int i =0;i<=length;i++) {
+			if (s.charAt(i)!=',') {
+				split = split+split.charAt(i);
+			} else {
+				i =i +1;
+				temp[index] =split;
+				index++;
+			}
+		}
+		
+		temp[index]=split;
+		for (String s:temp) {
+			System.out.println("Spliting value :"+s);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		WithoutSplit objWithoutSplit = new WithoutSplit();
+		
+		objWithoutSplit.withOutSliptFunction();
+	}
+
+}
